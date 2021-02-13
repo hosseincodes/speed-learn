94d716d73aedebe4cf25c2aad615c4ce1634c443
Hossein Akbari <hosseinakbari506@gmail.com>
2021-02-13 23:12:30 +0330

    responsive product page

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -38,21 +38,24 @@ $product = wc_get_product(get_the_id());
 
 
         <div class="row wwsh-cource-area">
-            <h1 class="cource-title"><?php the_title(); ?></h1>
-            <div class="section-address-p">
-                <div class="broadc">
-                    <?php echo woocommerce_breadcrumb(); ?>
+            <div class="cource-title-box">
+                <h1 class="cource-title"><?php the_title(); ?></h1>
+                <div class="section-address-p">
+                    <div class="broadc">
+                        <?php echo woocommerce_breadcrumb(); ?>
+                    </div>
                 </div>
             </div>
 
-            <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 col-12 course-content">
+            <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 col-12">
+                <div class="course-content">
 
-                <?php while ( have_posts() ) : the_post(); ?>
+                    <?php while ( have_posts() ) : the_post(); ?>
 
-                <?php wc_get_template_part( 'content', 'single-product' ); ?>
+                    <?php wc_get_template_part( 'content', 'single-product' ); ?>
 
-                <?php endwhile; // end of the loop. ?>
-
+                    <?php endwhile; // end of the loop. ?>
+                </div>
             </div>
             <div class="col-lg-3 col-md-3 col-sm-0 col-xs-0">
                 <aside class="sidebar d-none d-lg-block sidebar-products">
@@ -101,49 +104,50 @@ $product = wc_get_product(get_the_id());
 
         </div>
 
-        <div class="row more-courses">
-            <h3 class="all-areas-title">دوره های پیشنهادی</h3>
+        <div class="col-12">
+            <div class="more-courses">
+                <h3 class="all-areas-title">دوره های پیشنهادی</h3>
 
-            <div class="row slyder-1">
+                <div class="row slyder-1">
 
-                <?php global $product; $crosssell_ids = $product->get_upsell_ids();
+                    <?php global $product; $crosssell_ids = $product->get_upsell_ids();
 
 $args = array('post_type'=>'product','posts_per_page'=>4,'post__in'=>$crosssell_ids,'orderby'=>'rand');
 $loop = new WP_Query($args); if($loop->have_posts()){ ?>
 
-                <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+                    <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
-                <div class="col-lg-3 col-sm-3 col-12 course-box">
-                    <div class="thumbnail courses blog">
-                        <a href=<?php echo the_permalink(); ?>><?php the_post_thumbnail( 'wwshthumb' ); ?></a>
-                        <div class="caption">
-                            <a class="caption-tittle" href="<?php echo the_permalink(); ?>"><?php the_title() ?></a>
-                            <div class="row information-course-box">
-                                <div class="col-md-8 col-xs-6 col-sm-6 teacher">
-                                    <a href="<?php echo get_author_posts_url($author_id,$user_nicename); ?>"><i
-                                            style="margin-left: 5px;" class="fas fa-user"></i><?php the_author() ?></a>
+                    <div class="col-lg-3 col-sm-3 col-12 course-box">
+                        <div class="thumbnail courses blog">
+                            <a href=<?php echo the_permalink(); ?>><?php the_post_thumbnail( 'wwshthumb' ); ?></a>
+                            <div class="caption">
+                                <a class="caption-tittle" href="<?php echo the_permalink(); ?>"><?php the_title() ?></a>
+                                <div class="row information-course-box">
+                                    <div class="col-md-8 col-xs-6 col-sm-6 teacher">
+                                        <a href="<?php echo get_author_posts_url($author_id,$user_nicename); ?>"><i
+                                                style="margin-left: 5px;"
+                                                class="fas fa-user"></i><?php the_author() ?></a>
+                                    </div>
+                                    <div class="col-md-4 col-xs-6 col-sm-6 users">
+                                        <?php global $product; $units_sold = get_post_meta( $product->id, 'total_sales', true ); echo ''. sprintf( __( '%s', 'woocommerce' ), $units_sold ).''; ?>
+                                        مشترک</div>
                                 </div>
-                                <div class="col-md-4 col-xs-6 col-sm-6 users">
-                                    <?php global $product; $units_sold = get_post_meta( $product->id, 'total_sales', true ); echo ''. sprintf( __( '%s', 'woocommerce' ), $units_sold ).''; ?>
-                                    مشترک</div>
-                            </div>
-                            <hr style="color: #e9ebed; margin: 0px;">
-                            <div class="price-box">
-                                <div class="price-background">
-                                    <?php woocommerce_template_loop_price() ?>
+                                <hr style="color: #e9ebed; margin: 0px;">
+                                <div class="price-box">
+                                    <div class="price-background">
+                                        <?php woocommerce_template_loop_price() ?>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+
+                    <?php endwhile; ?>
+
+                    <?php } wp_reset_postdata(); ?>
+
                 </div>
-
-                <?php endwhile; ?>
-
-                <?php } wp_reset_postdata(); ?>
-
             </div>
-
-
         </div>
 
     </div>
