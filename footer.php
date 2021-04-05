077b6a63d9a0f1fd2276b5bcc6c7262fbd30ce70
Hossein Akbari <hosseinakbari506@gmail.com>
2021-04-05 16:26:18 +0430

    Improved multiple links

--- footer.php
+++ footer.php
@@ -8,29 +8,28 @@
         <div class="container">
             <div class="col-md-4 col-xs-12 col-sm-6">
                 <h4 class="footer-themes">از این مجموعه</h4>
-                <p class="footer-p"><i class="fas fa-circle footer-p-circle"></i> <a target="_blank" href="http://mathbot.ir">لذت
-                        یادگیری ریاضی</a></p>
                 <p class="footer-p"><i class="fas fa-circle footer-p-circle"></i> <a target="_blank" href="http://shenoto.com/channel/FiveHundredSix">پادکست برنامه نویسی</a></p>
-                <p class="footer-p"><i class="fas fa-circle footer-p-circle"></i> <a target="_blank" href="http://hosseinakbari506.ir">سفارش طراحی وب سایت</a></p>
+                <p class="footer-p"><i class="fas fa-circle footer-p-circle"></i> <a target="_blank" href="https://hosseincodes.github.io/my-personal-website/">سفارش طراحی وب سایت</a></p>
+                <p class="footer-p"><i class="fas fa-circle footer-p-circle"></i> <a target="_blank" href="http://mathbot.ir">انجمن پرسش و پاسخ ریاضی</a></p>
             </div>
             <div class="col-md-4 col-xs-12 col-sm-6">
-                <h4 class="footer-themes">دسترسی سریع</h4>
-                <p class="footer-p"><i class="fas fa-circle footer-p-circle"></i> <a href="http://speed-learn.ir/vlog">ولاگ</a></p>
-                <p class="footer-p"><i class="fas fa-circle footer-p-circle"></i> <a href="http://speed-learn.ir/cart">سبد خرید</a></p>
-                <p class="footer-p"><i class="fas fa-circle footer-p-circle"></i> <a href="http://speed-learn.ir/my-account">حساب کاربری</a></p>
+                <h4 class="footer-themes">لورم ایپسوم </h4>
+                <p class="footer-p"><i class="fas fa-circle footer-p-circle"></i> <a href="#">لورم ایپسوم متن ساختگی </a></p>
+                <p class="footer-p"><i class="fas fa-circle footer-p-circle"></i> <a href="#">لورم ایپسوم متن ساختگی </a></p>
+                <p class="footer-p"><i class="fas fa-circle footer-p-circle"></i> <a href="#">لورم ایپسوم متن ساختگی </a></p>
             </div>
             <div class="col-md-4 col-xs-12 col-sm-6">
-                <h4 class="footer-themes">لینک های مفید</h4>
-                <p class="footer-p"><i class="fas fa-circle footer-p-circle"></i> <a target="_blank" href="http://codepen.io/project/editor">ویرایشگر آنلاین کد</a></p>
-                <p class="footer-p"><i class="fas fa-circle footer-p-circle"></i> <a target="_blank" href="http://flerbo.ir/category/%D8%A7%D8%B3%D8%AA%DB%8C%DA%A9%D8%B1%D9%87%D8%A7%DB%8C-%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D9%87%E2%80%8C%D9%86%D9%88%DB%8C%D8%B3%DB%8C">استیکر های برنامه نویسی</a></p>
-                <p class="footer-p"><i class="fas fa-circle footer-p-circle"></i> <a target="_blank" href="http://barnamenevis.ir/">موتور جستجو برنامه نویسان</a></p>
+                <h4 class="footer-themes">لورم ایپسوم </h4>
+                <p class="footer-p"><i class="fas fa-circle footer-p-circle"></i> <a href="#">لورم ایپسوم متن ساختگی </a></p>
+                <p class="footer-p"><i class="fas fa-circle footer-p-circle"></i> <a href="#">لورم ایپسوم متن ساختگی </a></p>
+                <p class="footer-p"><i class="fas fa-circle footer-p-circle"></i> <a href="#">لورم ایپسوم متن ساختگی </a></p>
             </div>
         </div>
     </div>
     <div class="footer-bottom">
         <div class="container">
             <div class="col-md-6 col-xs-12">
-                <div class="copy-right"> تمام حقوق سایت برای اسپید لرن محفوظ می باشد / طراحی و توسعه توسط <a target="_blank" href="http://hosseinakbari506.ir">حسین اکبری</a></div>
+                <div class="copy-right"> تمام حقوق سایت برای اسپید لرن محفوظ می باشد / طراحی و توسعه توسط <a target="_blank" href="https://hosseincodes.github.io/my-personal-website/">حسین اکبری</a></div>
             </div>
             <div class="col-md-6 col-xs-12">
                 <div class="social-networks">
